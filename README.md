eb67c7d1563e0d4762c3b193e212dcd70cbbd50f
Nicolas VENTER <thedeal78@hotmail.fr>
2025-02-07 15:44:40 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MPT-V5-Demo
+
+### New features
+
+- card display
+- mobile responsiveness
+- single field filter (with Auto-Completion TODO)
+- better input reactivity (scroll + search)
+- compact display (TODO)
+
+### Expected features
+
+- increased speed
---
+++ docs/index.chunk-s3rev289.js
@@ -0,0 +1,5 @@
+import{F as q,I as A,Y as H,Z as g,ca as k}from"./index.chunk-gzmn61ft.js";var B=(h)=>{return(I)=>k(H,{fallback:!0,children:k(h,{...I},void 0,!1,void 0,this)},void 0,!1,void 0,this)},F=(h)=>(G)=>G[h],K=B(g(()=>import("./pages/Coordinator.chunk-ksw50631.js").then(F("CoordinatorPage")))),M=B(g(()=>import("./components/coordinator/CoordinatorCardView.chunk-bqbay3yp.js").then(F("CoordinatorCardView")))),N=B(g(()=>import("./components/coordinator/CoordinatorTableView.chunk-b5k4kjge.js").then(F("CoordinatorTableView"))));
+export{K as a,M as b,N as c};
+
+//# debugId=970EFA0EABF2D00164756E2164756E21
+//# sourceMappingURL=index.chunk-s3rev289.js.map
